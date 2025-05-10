8de5ca44b9f9d2285fd4ca6373d64d8d51c6b2da
ardhikakrsh <ardhikakrishna@gmail.com>
2025-05-10 21:10:37 +0700

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:lotso_app/view/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lotsoApp/firebase_options.dart';
+import 'package:lotsoApp/view/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
